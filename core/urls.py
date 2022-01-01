94b1284f783e8985272a27618a0e2796a274ac89
bugbytes-io <bugbytes.io@gmail.com>
2022-01-01 17:07:49 +0000

    Alpine Introduction

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from core import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('people/', views.people, name='people')
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+def people(request):
+    people = [
+        {"name": 'Mark', "image": 'https://via.placeholder.com/150'},
+        {"name": 'Jeff', "image": 'https://via.placeholder.com/150'}
+    ]
+    import time
+    time.sleep(1.)
+    return JsonResponse(people, safe=False)
